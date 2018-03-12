8966ac2c3e8bf99c965bc0fc9fb7a5607b8dda5d
notdrone <notdrone@gmail.com>
2018-03-12 18:42:43 +0530

    fix balance-reducer
    initial state was not being considered

--- src/api/balances-api.ts
+++ src/api/balances-api.ts
@@ -4,10 +4,10 @@ export interface IBalance {
 }
 export const getEkoBalanceApi = (): IBalance => ({
   amount: 90000,
-  timestamp: '7:12 pm 29 Mar 2018',
+  timestamp: '7:00PM 12/3/18',
 })
 
 export const getNodalBalanceApi = (): IBalance => ({
   amount: 39000,
-  timestamp: '7:14 pm 29 Mar 2018',
+  timestamp: '7:01PM 12/3/17',
 })
--- src/reducers/balance-reducer.ts
+++ src/reducers/balance-reducer.ts
@@ -4,12 +4,14 @@ import { balanceActions } from '../actions/constants-actions'
 export default function(state = {}, action: IBalanceAction) {
   if (action.type === balanceActions.eko) {
     return {
-      nodal: action.nodal,
+      ...state,
+      eko: action.eko,
     }
   }
   if (action.type === balanceActions.nodal) {
     return {
-      eko: action.eko,
+      ...state,
+      nodal: action.nodal,
     }
   }
   return state
