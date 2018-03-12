6b470289859daaec2fe4eba9f493fa364ef55fc6
notdrone <notdrone@gmail.com>
2018-03-12 19:33:57 +0530

    remove dispatcher in transactionContainer

--- src/screens/Transactions/TransactionsContainer.ts
+++ src/screens/Transactions/TransactionsContainer.ts
@@ -1,14 +1,8 @@
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { getAllTransactions } from '../../actions/transactions-actions'
 import { IReduxState } from '../../store/initial-state'
 import Transactions from './Transactions'
 const mapStateToProps = (state: IReduxState) => {
   return { transactions: state.transactions }
 }
 
-const mapDispatchToProps = (dispatch: any) => {
-  bindActionCreators({ getAll: getAllTransactions }, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Transactions)
+export default connect(mapStateToProps)(Transactions)
