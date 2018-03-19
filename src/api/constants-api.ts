015f63a3e378031e76944ec6277f2901cb410d9d
notdrone <notdrone@gmail.com>
2018-03-19 15:59:44 +0530

    generate base url from process.env

--- src/api/constants-api.ts
+++ src/api/constants-api.ts
@@ -1,18 +1,32 @@
-export const basePath = 'http://localhost:8080/api/v1/'
+const basePath = () => {
+  const env = process.env
+  // tslint:disable-next-line
+  console.log('env', env, process.env)
+  if (env.NODE_ENV === 'development') {
+    return 'http://localhost:8080/api/v1/'
+  }
+  if (env.REACT_APP_BASE_URL === 'beta') {
+    return 'https://beta.91paisa.com/api/v1/'
+  }
+  if (env.REACT_APP_BASE_URL === 'prod') {
+    return 'https://91paisa.com/api/v1/'
+  }
+  return
+}
 
 export const CustomerPath = {
-  all: `${basePath}customer/all/`,
-  create: `${basePath}customer/create/`,
-  detail: `${basePath}customer/get/`,
-  update: `${basePath}customer/update/`,
-  verify: `${basePath}customer/verify/`,
+  all: `${basePath()}customer/all/`,
+  create: `${basePath()}customer/create/`,
+  detail: `${basePath()}customer/get/`,
+  update: `${basePath()}customer/update/`,
+  verify: `${basePath()}customer/verify/`,
 }
 
 export const TransactionPath = {
-  all: `${basePath}transaction/all/`,
+  all: `${basePath()}transaction/all/`,
 }
 
 export const AuthPath = {
-  login: `${basePath}reviewer/login/`,
-  logout: `${basePath}reviewer/logout/`,
+  login: `${basePath()}reviewer/login/`,
+  logout: `${basePath()}reviewer/logout/`,
 }
