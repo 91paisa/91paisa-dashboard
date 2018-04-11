e8eedd15f429bc5f32672b6447e29a67075ea6d1
notdrone <notdrone@gmail.com>
2018-04-12 03:13:02 +0530

    fetch beneficiary when component Updates
    Beneficiary Container was being mounted once after switching to master-detail.

--- src/screens/CustomerDetail/Beneficiary/BeneficiariesListContainer.tsx
+++ src/screens/CustomerDetail/Beneficiary/BeneficiariesListContainer.tsx
@@ -2,38 +2,42 @@ import { Component, default as React } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { getBeneficiaryAction } from '../../../actions/customer-actions'
+import { IBeneficiary } from '../../../api/customer-api'
 import { IReduxState } from '../../../store/initial-state'
 import BeneficiariesList from './BeneficiariesList'
-//
-// interface IReduxProps {
-//   getBeneficiaryAction: any
-//   beneficiaries: IBeneficiary[] | undefined
-// }
 
-interface IOwnProps {
+interface IProps {
   customerPhone: string
   customerIndex: number
+  getBeneficiaryAction: (customerPhone: string) => void
+  beneficiaries: IBeneficiary[]
 }
 
-class BeneficiariesListContainer extends Component<IOwnProps | any> {
-  public componentDidMount () {
+class BeneficiariesListContainer extends Component<IProps> {
+  public componentDidMount() {
     this.props.getBeneficiaryAction(this.props.customerPhone)
   }
 
-  public render () {
-    return <BeneficiariesList beneficiaries={this.props.beneficiaries}/>
+  public componentWillUpdate(newProps: IProps) {
+    if (newProps.customerPhone !== this.props.customerPhone) {
+      this.props.getBeneficiaryAction(newProps.customerPhone)
+    }
+  }
+
+  public render() {
+    return <BeneficiariesList beneficiaries={this.props.beneficiaries} />
   }
 }
 
-const mapStateToProps = (state: IReduxState, props: IOwnProps) => {
+const mapStateToProps = (state: IReduxState, props: any) => {
   return {
     beneficiaries: state.customers[props.customerIndex].beneficiaries,
     customerPhone: props.customerPhone,
   }
 }
 const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators({getBeneficiaryAction}, dispatch)
+  return bindActionCreators({ getBeneficiaryAction }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(
-  BeneficiariesListContainer,
+  BeneficiariesListContainer as any,
 )
